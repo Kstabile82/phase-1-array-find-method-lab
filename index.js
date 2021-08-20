e8ab4176270db3ccb7341dd74a9e6d02dbf6f995
KarinaStabile <karina.stabile@gmail.com>
2021-08-19 22:33:53 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(record) {
+    for (let i = 0; i < record.length; i++) {
+        if (record[i]['result'] === 'W') {
+            return record[i]['year']; 
+        }
+    }
+}
+record.find(superbowlWin); 
